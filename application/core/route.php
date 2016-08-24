4722f76ca94e54cde6ffdddd680d6319c4d037ff
Boris Gribovskiy <boris.gribovskiy@cyberhull.com>
2016-08-24 18:41:05 +0300

    v1.0.4 fixed Route class

--- application/core/route.php
+++ application/core/route.php
@@ -29,41 +29,40 @@ class Route
 		}
 
 		// добавляем префиксы
-		$model_name = 'Model_'.$controller_name;
-		$controller_name = 'Controller_'.$controller_name;
+		$model_name = 'models\\'.$controller_name;
+		$controller_name = 'controllers\\'.$controller_name;
 		$action_name = 'action_'.$action_name;
 
-		/*
-		echo "Model: $model_name <br>";
-		echo "Controller: $controller_name <br>";
-		echo "Action: $action_name <br>";
-		*/
+
+//		echo "Model: $model_name <br>";
+//		echo "Controller: $controller_name <br>";
+//		echo "Action: $action_name <br>";
 
 		// подцепляем файл с классом модели (файла модели может и не быть)
 
-		$model_file = strtolower($model_name).'.php';
-		$model_path = "application/models/".$model_file;
-		if(file_exists($model_path))
-		{
-			include "application/models/".$model_file;
-		}
-
-		// подцепляем файл с классом контроллера
-		$controller_file = strtolower($controller_name).'.php';
-		$controller_path = "application/controllers/".$controller_file;
-		if(file_exists($controller_path))
-		{
-			include "application/controllers/".$controller_file;
-		}
-		else
-		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
-			Route::ErrorPage404();
-		}
-		
+//		$model_file = strtolower($model_name).'.php';
+//		$model_path = "application/models/".$model_file;
+//		if(file_exists($model_path))
+//		{
+//			include "application/models/".$model_file;
+//		}
+//
+//		// подцепляем файл с классом контроллера
+//		$controller_file = strtolower($controller_name).'.php';
+//		$controller_path = "application/controllers/".$controller_file;
+//		if(file_exists($controller_path))
+//		{
+//			include "application/controllers/".$controller_file;
+//		}
+//		else
+//		{
+//			/*
+//			правильно было бы кинуть здесь исключение,
+//			но для упрощения сразу сделаем редирект на страницу 404
+//			*/
+//			Route::ErrorPage404();
+//		}
+//
 		// создаем контроллер
 		$controller = new $controller_name;
 		$action = $action_name;
@@ -81,7 +80,7 @@ class Route
 	
 	}
 
-	function ErrorPage404()
+	public static function ErrorPage404()
 	{
         $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
         header('HTTP/1.1 404 Not Found');
